1927aaf7fa389e5b872b54a1aa36e56d2d4dfd65
Joseph McGreene <josephgreene693@gmail.com>
2020-08-07 13:21:25 -0700

    Add a system for identifying accidentals

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ class Key {
   }
 };
 
-const noteNames = [
+const noteNamesHTML = [
   'C&#9837;', 'C&#9838;', 'C&#9839;',
   'D&#9837;', 'D&#9838;', 'D&#9839;',
   'E&#9837;', 'E&#9838;', 'E&#9839;',
@@ -18,6 +18,16 @@ const noteNames = [
   'G&#9837;', 'G&#9838;', 'G&#9839;',
   'A&#9837;', 'A&#9838;', 'A&#9837;',
   'B&#9837;', 'B&#9838;', 'B&#9839;'
+];
+
+const noteNames = [
+  'Cflat', 'Cnatural', 'Csharp',
+  'Dflat', 'Dnatural', 'Dsharp',
+  'Eflat', 'Enatural', 'Esharp',
+  'Fflat', 'Fnatural', 'Fsharp',
+  'Gflat', 'Gnatural', 'Gsharp',
+  'Aflat', 'Anatural', 'Asharp',
+  'Bflat', 'Bnatural', 'Bsharp'
 ];
 
 // const noteNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
@@ -37,12 +47,16 @@ let ul = document.querySelector('#chordSuggestionList');
 inputButton.addEventListener('click', () => {
   if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) ) {
     let newTonic = chordInput.value.toUpperCase() + accidentalSelect.value;
-    while ( chordInput.value.toUpperCase() !== noteNames[0] ) {
+    while ( newTonic !== noteNames[0] ) {
+      let shiftNoteHTML = noteNamesHTML.shift();
       let shiftNote = noteNames.shift();
+
+      noteNamesHTML.push(shiftNoteHTML);
       noteNames.push(shiftNote);
     }
   }
   let quickTest = document.createElement('li');
   ul.appendChild(quickTest);
-  quickTest.innerHTML = `${noteNames}`;
+  console.log(noteNames);
+  quickTest.innerHTML = `${noteNamesHTML}`;
 });
