872395a8b846774116a42f309380b3c8d89cbc76
Joseph McGreene <josephgreene693@gmail.com>
2020-08-10 13:51:23 -0700

    Add getKeyAccidentals() and add it to establishKey()

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,20 @@
-class Key {
-  constructor(tonic, supertonic, mediant, subdominant, dominant, submediant, leadingTone) {
-    this.tonic = tonic,
-    this.supertonic = supertonic,
-    this.mediant = mediant,
-    this.subdominant = subdominant,
-    this.dominant = dominant,
-    this.submediant = submediant,
-    this.leadingTone = leadingTone
-  }
-};
-
-const noteNamesHTML = [
-  'C&#9838;', 'C&#9839;',
-  'D&#9837;', 'D&#9838;',
-  'E&#9837;', 'E&#9838;',
-  'F&#9838;', 'F&#9839;',
-  'G&#9837;', 'G&#9838;',
-  'A&#9837;', 'A&#9838;',
-  'B&#9837;', 'B&#9838;',
-];
 const noteNames = [
   'Cnatural', 'Csharp',
-  'Dflat', 'Dnatural',
+  'Dflat', 'Dnatural', 'Dsharp',
   'Eflat', 'Enatural',
   'Fnatural', 'Fsharp',
-  'Gflat', 'Gnatural',
-  'Aflat', 'Anatural',
+  'Gflat', 'Gnatural', 'Gsharp',
+  'Aflat', 'Anatural', 'Asharp',
   'Bflat', 'Bnatural'
 ];
 
-const sharpKeys = ['Gnatural', 'Dnatural', 'Anatural', 'Enatural', 'Bnatural', 'Fsharp', 'Csharp'];
-const flatKeys = ['Fnatural', 'Bflat', 'Eflat', 'Aflat', 'Dflat', 'Gflat'];
-const cMajor = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const sharpKeysMajor = ['Gnatural', 'Dnatural', 'Anatural', 'Enatural', 'Bnatural', 'Fsharp', 'Csharp'];
+const flatKeysMajor = ['Fnatural', 'Bflat', 'Eflat', 'Aflat', 'Dflat', 'Gflat'];
+
+const sharpKeysMinor = ['Enatural', 'Bnatural', 'Fsharp', 'Csharp', 'Gsharp', 'Dsharp', 'Asharp'];
+const flatKeysMinor = ['Dnatural', 'Gnatural', 'Cnatural', 'Fnatural', 'Bflat', 'Eflat', 'Aflat'];
+
+const cMajorAMinor = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 const orderOfSharps = ['F', 'C', 'G', 'D', 'A', 'E', 'B'];
 const orderOfFlats = ['B', 'E', 'A', 'D', 'G', 'C', 'F'];
@@ -40,38 +23,68 @@ const chordInput = document.querySelector('#chordInput');
 const inputButton = document.querySelector('#chordInputButton');
 const accidentalSelect = document.querySelector('#accidentalSelect');
 const qualitySelect = document.querySelector('#qualitySelect');
-let suggestionList = document.querySelector('#chordSuggestionList');
+let chordSuggestions = document.querySelector('#chordSuggestionList');
+let key;
+let tonic;
+let tonicAccidentals;
 
-    /**
-     * Establishes the key used to suggest chords once the user presses the inputButton.
-     * @return  [Array]    an array containing the new key.
-     */
+  /**
+   * Returns an array containing the appropriate number of sharps or flats for a given key and what those sharps are.
+   * callback in esatblishKey()
+   * @param [keysArray]         array of notes to pull from
+   * @param [orderOfArray]      array orderOfSharps or orderOfFlats to use to know what sharps/flats to use
+   * @param [accidentalHTML]    HTML hex code for the necessary accidental
+   * @return  [Array]    an array containing the sharps/flats.
+   */
+function getKeyAccidentals(keysArray, orderOfArray, accidentalHTML) {
+  let numOfAccidentals = keysArray.indexOf(tonic) + 1;
+  tonicAccidentals = orderOfArray.filter( i => orderOfArray.indexOf(i) < numOfAccidentals );
+  tonicAccidentals.forEach( (val, idx) => tonicAccidentals[idx] = `${val}${accidentalHTML}` );
+  return tonicAccidentals;
+}
 
+  /**
+   * Establishes the key used to suggest chords once the user presses the inputButton, or tells user to input something else.
+   * @return  [Array]    an array containing the new key.
+   */
 function establishKey() {
-  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) ) {
-    let tonic = chordInput.value.toUpperCase() + accidentalSelect.value;
-    let key;
+  // for Major keys
+  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value === 'major' ) {
+    tonic = chordInput.value.toUpperCase() + accidentalSelect.value;
     // If the user input is a key with sharps:
-    if ( sharpKeys.includes(tonic) ) {
-      let numOfSharps = sharpKeys.indexOf(tonic) + 1; /** 1 for Gnatural, 2 for Dnatural, etc. */
-      let tonicSharps = orderOfSharps.filter( i => orderOfSharps.indexOf(i) < numOfSharps );
-      tonicSharps.forEach( (val, idx) => tonicSharps[idx] = `${val}&#9839;` ); /** HTML Hex for sharp symbol */
+    if ( sharpKeysMajor.includes(tonic) ) {
+      getKeyAccidentals(sharpKeysMajor, orderOfSharps, '&#9839;');
     } 
     // If the user input is a key with flats:
-    else if ( flatKeys.includes(tonic) ) {
-      let numOfFlats = flatKeys.indexOf(tonic) + 1; /** 1 for Fnatural, 2 for Bflat, etc. */
-      let tonicFlats = orderOfFlats.filter( i => orderOfFlats.indexOf(i) < numOfFlats );
+    else if ( flatKeysMajor.includes(tonic) ) {
+      getKeyAccidentals(flatKeysMinor, orderOfFlats, '&#9837;')
     }
     else if ( tonic === 'Cnatural' ) {
-      key = cMajor;
+      key = cMajorAMinor;
+    }
+  }
+
+  // for minor keys
+  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value === 'minor' ) {
+    let tonic = chordInput.value.toUpperCase() + accidentalSelect.value;
+    // If the user input is a key with sharps:
+    if ( sharpKeysMinor.includes(tonic) ) {
+      getKeyAccidentals(sharpKeysMinor, orderOfSharps, '&#9839;')
     }
 
-  } else {
+    else if ( flatKeysMinor.includes(tonic) ) {
+      getKeyAccidentals(flatKeysMinor, orderOfFlats, '&#9837;')
+    }
+
+    else if ( tonic === 'Anatural' ) {
+      key = cMajorAMinor.sort();
+    }
+  } 
+  
+  else {
     let callingBS = document.createElement('li');
-    callingBS.innerHTML = `Umm... that key will make people hate you. Try again.`;
-    callingBS.className = 'suggestion-list';
-    suggestionList = callingBS.parentNode;
-    suggestionList.appendChild(callingBS);
+    callingBS.innerHTML = `Umm... That key will make people hate you. Try again.`;
+    chordSuggestions.appendChild(callingBS);
   }
 }
 
@@ -79,4 +92,6 @@ function establishKey() {
 ============================== Event Listeners ==============================
 */
 
-inputButton.addEventListener( 'click', () => establishKey() );
+inputButton.addEventListener( 'click', () => {
+  establishKey();
+});
