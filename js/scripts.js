1fb7f16f6b3e5f4492472bc7761305e68e021ff0
Joseph McGreene <josephgreene693@gmail.com>
2020-08-11 15:27:26 -0700

    Finish establishKey() and begin intracting with the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ const flatKeysMajor = ['Fnatural', 'Bflat', 'Eflat', 'Aflat', 'Dflat', 'Gflat'];
 const sharpKeysMinor = ['Enatural', 'Bnatural', 'Fsharp', 'Csharp', 'Gsharp', 'Dsharp', 'Asharp'];
 const flatKeysMinor = ['Dnatural', 'Gnatural', 'Cnatural', 'Fnatural', 'Bflat', 'Eflat', 'Aflat'];
 
-const scaleNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+let scaleNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 const orderOfSharps = ['F&#9839;', 'C&#9839;', 'G&#9839;', 'D&#9839;', 'A&#9839;', 'E&#9839;', 'B&#9839;'];
 const orderOfFlats = ['B&#9837;', 'E&#9837;', 'A&#9837;', 'D&#9837;', 'G&#9837;', 'C&#9837;', 'F&#9837;'];
@@ -29,7 +29,7 @@ let tonic;
 let tonicAccidentals;
 
   /**
-   * Returns an array containing the appropriate number of sharps or flats for a given key and what those sharps are.
+   * Returns an array containing the appropriate number of sharps or flats for a given key and what those sharps or flats are.
    * callback in esatblishKey()
    * @param [keysArray]         array of notes to pull from
    * @param [orderOfArray]      array orderOfSharps or orderOfFlats to use to know what sharps/flats to use
@@ -42,7 +42,7 @@ function getKeyAccidentals(keysArray, orderOfArray) {
 }
 
   /**
-   * Finishes constructing an array containing the notes necessary to return a key in establishKey();
+   * Uses array from getKeyAccidentals() to finish constructing an array containing the notes necessary to return a key in establishKey();
    * @return  [Array]    an array containing the new key.
    */
 function finishKey() {
@@ -50,11 +50,21 @@ function finishKey() {
     let shiftNote = scaleNotes.shift();
     scaleNotes.push(shiftNote);
   }
+  // This loop finds the appropriate note(s) to replace in scaleNotes[] from the corresponding note(s) in tonicAccidentals[].
   for ( let i = 0; i < tonicAccidentals.length; i++ ) {
   scaleNotes.splice( scaleNotes.indexOf(tonicAccidentals[i].charAt(0)) , 1, tonicAccidentals[i]);
   }
-  console.log(scaleNotes);
   return scaleNotes;
+}
+
+  /**
+   * A logic statement checking to make sure the user inputs a letter A through G
+   * @return  Boolean    whether the user's input is a music note or not.
+   */
+function notAMusicNote() {
+  if ( chordInput.value.toUpperCase() !== 'A' && chordInput.value.toUpperCase() !== 'B' && chordInput.value.toUpperCase() !== 'C' && chordInput.value.toUpperCase() !== 'D' && chordInput.value.toUpperCase() !== 'E' && chordInput.value.toUpperCase() !== 'F' && chordInput.value.toUpperCase() !== 'G' ) {
+    return true;
+  } else { return false; }
 }
 
   /**
@@ -64,34 +74,38 @@ function finishKey() {
    */
 function establishKey() {
   // for Major keys
-  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value === 'major' ) {
+  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value.toLowerCase() === 'major' ) {
     tonic = chordInput.value.toUpperCase() + accidentalSelect.value;
     // If the user input is a key with sharps:
     if ( sharpKeysMajor.includes(tonic) ) {
       getKeyAccidentals(sharpKeysMajor, orderOfSharps);
       finishKey();
+      key = scaleNotes;
     } 
     // If the user input is a key with flats:
     else if ( flatKeysMajor.includes(tonic) ) {
       getKeyAccidentals(flatKeysMajor, orderOfFlats);
       finishKey();
+      key = scaleNotes;
     }
     else if ( tonic === 'Cnatural' ) {
       key = scaleNotes;
     }
   }
   // for minor keys
-  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value === 'minor' ) {
+  if ( noteNames.includes(chordInput.value.toUpperCase() + accidentalSelect.value) && qualitySelect.value.toLowerCase() === 'minor' ) {
     tonic = chordInput.value.toUpperCase() + accidentalSelect.value;
     // If the user input is a key with sharps:
     if ( sharpKeysMinor.includes(tonic) ) {
       getKeyAccidentals(sharpKeysMinor, orderOfSharps);
       finishKey();
+      key = scaleNotes;
     }
     // If the user input is a key with flats:
     else if ( flatKeysMinor.includes(tonic) ) {
       getKeyAccidentals(flatKeysMinor, orderOfFlats);
       finishKey();
+      key = scaleNotes;
     }
 
     else if ( tonic === 'Anatural' ) {
@@ -99,7 +113,7 @@ function establishKey() {
     }
   }
   // Errors
-  else if (  ) {
+  else if ( notAMusicNote() ) {
     let callingBS = document.createElement('li');
     callingBS.innerHTML = `That letter is not used in music. Try a letter A-G.`;
     chordSuggestions.appendChild(callingBS);
@@ -116,5 +130,27 @@ function establishKey() {
 */
 
 inputButton.addEventListener( 'click', () => {
-  establishKey();
+  if ( scaleNotes === ['C', 'D', 'E', 'F', 'G', 'A', 'B'] ) {
+    establishKey();
+  }
+  else if ( scaleNotes !== ['C', 'D', 'E', 'F', 'G', 'A', 'B'] ) {
+    scaleNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+    establishKey();
+  }
+
+  let keyAnnounce = document.createElement('h2');
+  let chordSuggest = document.createElement('h2');
+  let startOtherButton = document.createElement('button');
+  let suggestOtherButton = document.createElement('button');
+
+  keyAnnounce.innerHTML = `Well, start with <strong>${chordInput.value.toUpperCase()} ${qualitySelect.value}</strong>`;
+  startOtherButton.innerHTML = `Start on a different Chord`;
+  chordSuggest.innerHTML = `then try ${key[3]} or ${key[4]}`;
+  suggestOtherButton.innerHTML = `Try something different`;
+
+  chordSuggestions.appendChild(keyAnnounce);
+  keyAnnounce.appendChild(startOtherButton);
+  chordSuggestions.appendChild(chordSuggest);
+  chordSuggest.appendChild(suggestOtherButton);
 });
+
